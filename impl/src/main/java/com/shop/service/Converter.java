55d71e3e530e2e31cd5a9ca706cc6a6ca48f82c6
Анна Капранова <annakapranova@MacBook-Pro-Anna.local>
2019-09-24 23:29:35 +0300

    refactor

---
+++ impl/src/main/java/com/shop/service/Converter.java
@@ -0,0 +1,24 @@
+package com.shop.service;
+
+import com.shop.api.swagger.models.OrderDTO;
+import com.shop.model.Order;
+
+public class Converter {
+    public static OrderDTO convertToDTO(Order order) {
+        return new OrderDTO()
+                .id(order.getId())
+                .customerId(order.getCustomerId())
+                .status(order.getStatus())
+                .shippingType(order.getShippingType())
+                .total(order.getTotal());
+    }
+
+    public static Order convertToEntity(OrderDTO orderDTO) {
+        Order order = new Order();
+        order.setCustomerId(orderDTO.getCustomerId());
+        order.setShippingType(orderDTO.getShippingType());
+        order.setStatus(orderDTO.getStatus());
+        order.setPromocode(orderDTO.getPromocode());
+        return order;
+    }
+}
